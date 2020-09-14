13381fc4f09fd508119c08de7ddaee63a4efeda5
MIKIO ODA <oda.mikio@gmail.com>
2020-09-14 12:14:52 +0900

    ....

---
+++ src/app/platform/auth/username-dialog/username-dialog.directive.ts
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2019 7thCode.(http://seventh-code.com/)
+ * This software is released under the MIT License.
+ * opensource.org/licenses/mit-license.php
+ */
+
+"use strict";
+
+import {Directive, EventEmitter, HostListener, Output} from "@angular/core";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+import {InfoDialogComponent} from "../../base/components/info-dialog/info-dialog.component";
+import {UserNameDialogComponent} from "./username-dialog.component";
+
+@Directive({
+	selector: "[auth-username-button]",
+})
+
+export class UserNameDialogDirective {
+
+	/**
+	 *
+	 */
+	@Output() public complete = new EventEmitter<any>();
+
+	/**
+	 *
+	 * @param matDialog
+	 */
+	constructor(
+		private matDialog: MatDialog
+	) {
+	}
+
+	/**
+	 *
+	 * @param target
+	 */
+	@HostListener("click", ["$event.target"])
+	public onClick(target: any): void {
+
+		const resultDialogContent: any = {title: "Check mail", message: "Username Change Mail sent.", has_cancel: false};
+
+		const dialog: MatDialogRef<any> = this.matDialog.open(UserNameDialogComponent, {
+			width: "30%",
+			minWidth: "320px",
+			height: "fit-content",
+			data: {
+				content: {
+					title: "Username",
+					description: "Lorem ipsum...",
+					username: "",
+					password: "",
+					confirm_password: "",
+				},
+			},
+			disableClose: true,
+		});
+
+		dialog.afterClosed().subscribe((result: any) => {
+			if (result) {
+				const dialog: MatDialogRef<any> = this.matDialog.open(InfoDialogComponent, {
+					width: "30%",
+					minWidth: "320px",
+					height: "fit-content",
+					data: {content: resultDialogContent},
+					disableClose: true,
+				});
+
+				dialog.afterClosed().subscribe((result: any) => {
+					if (result) {
+						this.complete.emit(result);
+					}
+				});
+			}
+		});
+	}
+
+}
